4f4d540f7d23917c44d7ddfe4c9aead28c0207e6
Xaiodo <xaiodo.work@gmail.com>
2023-06-23 18:19:04 +0300

    Created auth service to check google sign in

---
+++ lib/src/services/auth_service.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  Future<void> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    //obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user!.displayName);
+    print(userCredential.user!.email);
+  }
+}
