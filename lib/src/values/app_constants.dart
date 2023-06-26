8084108343ad2b276fbe53b78b4d206da459bce5
Xaiodo <xaiodo.work@gmail.com>
2023-06-26 21:15:29 +0300

    Added bottom navigation constant strings

--- lib/src/values/app_constants.dart
+++ lib/src/values/app_constants.dart
@@ -4,7 +4,12 @@ const String googleLogoImagePath = 'assets/images/google_icon.png';
 const String facebookLogoImagePath = 'assets/images/facebook_icon.png';
 const String loginCatImagePath = 'assets/images/login_cat.png';
 
-// Texts
+// Login texts
 const String signInWithGoogleText = 'Sign in with Google';
 const String signInWithFacebookText = 'Sign in with Facebook';
 const String welcomeText = 'Welcome to Cat App';
+
+// Bottom navigation texts
+const String homeText = 'Home';
+const String favoritesText = 'Favorites';
+const String profileText = 'Profile';
