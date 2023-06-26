5927919fdada6715a0efa21012b333a6f94c5fed
Xaiodo <xaiodo.work@gmail.com>
2023-06-26 17:05:38 +0300

    Updated home screen to test logout feature

--- lib/src/pages/home/view/home_view.dart
+++ lib/src/pages/home/view/home_view.dart
@@ -1,10 +1,19 @@
+import 'package:cat_app/app/app_bloc/app_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: const Text('Home View'),
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              context.read<AppBloc>().logout();
+            },
+            child: const Text('Logout'),
+          ),
+        ),
       );
 }
