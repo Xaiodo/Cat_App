283ebfad14d0340efa4c33d3308ce9106b165a32
Xaiodo <xaiodo.work@gmail.com>
2023-06-27 15:40:08 +0300

    Fixed bottom navigation

--- lib/src/pages/bottom_navigation/view/bottom_navigation_view.dart
+++ lib/src/pages/bottom_navigation/view/bottom_navigation_view.dart
@@ -1,6 +1,7 @@
 import 'package:cat_app/src/pages/bottom_navigation/bottom_navigation_cubit/bottom_navigation_cubit.dart';
 import 'package:cat_app/src/pages/bottom_navigation/pages/home/home_view.dart';
 import 'package:cat_app/src/pages/bottom_navigation/pages/profile/profile_view.dart';
+import 'package:cat_app/src/values/app_colors.dart';
 import 'package:cat_app/src/values/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,25 +18,27 @@ class BottomNavigationView extends StatelessWidget {
           builder: (context) =>
               BlocBuilder<BottomNavigationCubit, BottomNavigationState>(
             builder: (context, state) => Scaffold(
+              backgroundColor: AppColors.backgroundColor,
               body: SafeArea(
                 child: Stack(
                   children: [
                     Offstage(
                       offstage: state.page != BottomNavPage.home,
-                      child: const Center(child: Home()),
+                      child: const HomeView(),
                     ),
                     Offstage(
                       offstage: state.page != BottomNavPage.favorites,
-                      child: const Center(child: FavoritesView()),
+                      child: const FavoritesView(),
                     ),
                     Offstage(
                       offstage: state.page != BottomNavPage.profile,
-                      child: const Center(child: ProfileView()),
+                      child: const ProfileView(),
                     ),
                   ],
                 ),
               ),
               bottomNavigationBar: NavigationBar(
+                height: 60,
                 destinations: const [
                   NavigationDestination(
                     icon: Icon(Icons.home_outlined),
