4a5a2f72d18649e8096c68f8cf9a4d02f6a1fc14
Xaiodo <xaiodo.work@gmail.com>
2023-06-27 15:38:05 +0300

    Created home view

--- lib/src/pages/bottom_navigation/pages/home/home_view.dart
+++ lib/src/pages/bottom_navigation/pages/home/home_view.dart
@@ -1,10 +1,61 @@
+import 'package:cat_app/src/pages/bottom_navigation/pages/home/repositories/cat_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+import '../../../../values/app_colors.dart';
+import '../../widgets/cat_item_widget.dart';
+import 'home_cubit/home_cubit.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: Text("Home"),
+  Widget build(BuildContext context) => BlocProvider(
+        create: (context) => HomeCubit(context.read<CatRepository>()),
+        child: Builder(
+          builder: (context) => BlocConsumer<HomeCubit, HomeState>(
+            listener: (context, state) {
+              if (state is HomeError) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(state.message),
+                  ),
+                );
+              }
+              if (state is HomeLoading) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Loading...'),
+                    duration: Duration(seconds: 1),
+                    backgroundColor: AppColors.primary,
+                  ),
+                );
+              }
+            },
+            builder: (context, state) =>
+                NotificationListener<ScrollNotification>(
+              onNotification: (notification) {
+                if (notification is ScrollEndNotification &&
+                    notification.metrics.extentAfter == 0) {
+                  context.read<HomeCubit>().loadMore();
+                }
+                return false;
+              },
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.8,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                padding: const EdgeInsets.all(10),
+                itemCount: state.cats.length,
+                itemBuilder: (context, index) =>
+                    CatItemWidget(cat: state.cats[index]),
+                physics: const BouncingScrollPhysics(),
+              ),
+            ),
+          ),
+        ),
       );
 }
