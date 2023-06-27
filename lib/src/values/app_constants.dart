c9b9af6464362f6b4527a603b661395f167ca12b
Xaiodo <xaiodo.work@gmail.com>
2023-06-27 15:41:48 +0300

    Added api path strings

--- lib/src/values/app_constants.dart
+++ lib/src/values/app_constants.dart
@@ -4,6 +4,13 @@ const String googleLogoImagePath = 'assets/images/google_icon.png';
 const String facebookLogoImagePath = 'assets/images/facebook_icon.png';
 const String loginCatImagePath = 'assets/images/login_cat.png';
 
+// Api paths
+const String catImagesApiKey =
+    'live_KlAMdpO5FxXRoixuPyitfqmIG2QKRyB8hjihB1IblkuKwkBtZqNhjQd5eNnoltJS';
+const String catFactsApiUrl = 'https://catfact.ninja/facts?limit=10';
+const String catImagesApiUrl =
+    'https://api.thecatapi.com/v1/images/search?limit=10';
+
 // Login texts
 const String signInWithGoogleText = 'Sign in with Google';
 const String signInWithFacebookText = 'Sign in with Facebook';
@@ -13,3 +20,5 @@ const String welcomeText = 'Welcome to Cat App';
 const String homeText = 'Home';
 const String favoritesText = 'Favorites';
 const String profileText = 'Profile';
+
+// Home texts
