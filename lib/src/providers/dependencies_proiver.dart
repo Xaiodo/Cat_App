392f03c85bab7919ef622d5ccbece34d2b3b79f7
Xaiodo <xaiodo.work@gmail.com>
2023-06-27 15:41:08 +0300

    Added new repository providers

--- lib/src/providers/dependencies_proiver.dart
+++ lib/src/providers/dependencies_proiver.dart
@@ -1,9 +1,13 @@
+import 'package:cat_app/src/pages/bottom_navigation/pages/home/repositories/cat_repository.dart';
+import 'package:cat_app/src/pages/bottom_navigation/pages/home/services/cat_facts_service.dart';
 import 'package:cat_app/src/services/auth_service.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../dao/auth_dao.dart';
+import '../pages/bottom_navigation/pages/home/services/cat_image_service.dart';
 import '../repositories/auth_repository.dart';
 
 class DependenciesProvider extends StatelessWidget {
@@ -13,15 +17,35 @@ class DependenciesProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MultiRepositoryProvider(providers: [
+        RepositoryProvider(create: (context) => Dio()),
         RepositoryProvider(create: (context) => AuthService()),
         RepositoryProvider(create: (context) => const FlutterSecureStorage()),
         RepositoryProvider(
-            create: (context) =>
-                AuthDao(storage: context.read<FlutterSecureStorage>())),
+          create: (context) => AuthDao(
+            storage: context.read<FlutterSecureStorage>(),
+          ),
+        ),
         RepositoryProvider(
           create: (context) => AuthRepository(
-              authService: context.read<AuthService>(),
-              authDao: context.read<AuthDao>()),
+            authService: context.read<AuthService>(),
+            authDao: context.read<AuthDao>(),
+          ),
         ),
+        RepositoryProvider(
+          create: (context) => CatFactsService(
+            dio: context.read<Dio>(),
+          ),
+        ),
+        RepositoryProvider(
+          create: (context) => CatImageService(
+            dio: context.read<Dio>(),
+          ),
+        ),
+        RepositoryProvider(
+          create: (context) => CatRepository(
+            catImageService: context.read<CatImageService>(),
+            catFactService: context.read<CatFactsService>(),
+          ),
+        )
       ], child: child);
 }
