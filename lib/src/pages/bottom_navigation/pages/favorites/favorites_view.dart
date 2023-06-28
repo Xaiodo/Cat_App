ca589554aad4cefa7fb34ccfc4accddd4c7a4352
Xaiodo <xaiodo.work@gmail.com>
2023-06-28 21:54:34 +0300

    Implemented handling no favorite cats

--- lib/src/pages/bottom_navigation/pages/favorites/favorites_view.dart
+++ lib/src/pages/bottom_navigation/pages/favorites/favorites_view.dart
@@ -11,6 +11,11 @@ class FavoritesView extends StatelessWidget {
   Widget build(BuildContext context) => BlocBuilder<CatCubit, CatState>(
         builder: (context, state) {
           final favoriteCats = context.read<CatCubit>().favoriteCats;
+          if (favoriteCats.isEmpty) {
+            return const Center(
+              child: Text('No favorite cats. Try to add some'),
+            );
+          }
           if (state is CatLoaded) {
             return GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -22,6 +27,7 @@ class FavoritesView extends StatelessWidget {
               padding: const EdgeInsets.all(10),
               itemCount: favoriteCats.length,
               itemBuilder: (context, index) => CatItemWidget(
+                heroTag: '${favoriteCats[index].id}favorite',
                 cat: favoriteCats[index],
               ),
               physics: const ClampingScrollPhysics(),
