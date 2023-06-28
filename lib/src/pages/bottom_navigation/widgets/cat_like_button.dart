656550a0cbcfe4b869b05b36c94f4e3042b85152
Xaiodo <xaiodo.work@gmail.com>
2023-06-28 21:49:52 +0300

    Created onTap field to provide from top level

--- lib/src/pages/bottom_navigation/widgets/cat_like_button.dart
+++ lib/src/pages/bottom_navigation/widgets/cat_like_button.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../pages/cat_cubit/cat_cubit.dart';
 
 class CatLikeButton extends StatelessWidget {
-  const CatLikeButton({super.key, required this.isLiked, required this.id});
-  final String id;
+  const CatLikeButton({super.key, required this.isLiked, this.onTap});
   final bool isLiked;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) => InkWell(
+        onTap: onTap,
         child: Icon(
           isLiked ? Icons.favorite : Icons.favorite_border,
           color: Colors.red,
         ),
-        onTap: () => context.read<CatCubit>().likeCat(id),
       );
 }
